25bd08c8b0b0210da5088832247dbb0ec33730e1
Karoline <karolgribeiro@gmail.com>
2023-08-02 22:47:20 -0300

    - user CRUD;

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
@@ -0,0 +1,65 @@
+package com.devsuperior.dscatalog.resources;
+
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<Page<UserDTO>> findAll(Pageable pageable) {
+        Page<UserDTO> findAllPaged = this.userService.findAllPaged(pageable);
+        return ResponseEntity.ok().body(findAllPaged);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(
+            @PathVariable Long id) {
+        UserDTO userDto = this.userService.findById(id);
+        return ResponseEntity.ok().body(userDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(
+            @RequestBody UserInsertDTO newUser){
+        UserDTO newUserDto = this.userService.createUser(newUser);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(newUserDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(newUserDto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> updateUser(
+            @PathVariable Long id,
+            @RequestBody UserDTO userUpdate){
+        UserDTO updatedUser = this.userService.updateUser(id, userUpdate);
+        return ResponseEntity.ok().body(updatedUser);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteUser(
+            @PathVariable Long id){
+        this.userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -0,0 +1,93 @@
+package com.devsuperior.dscatalog.services;
+
+import com.devsuperior.dscatalog.dto.RoleDTO;
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.entities.Role;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.RoleRepository;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Transactional(readOnly = true)
+    public Page<UserDTO> findAllPaged(Pageable pageRequest) {
+        Page<User> pagedList = this.userRepository.findAll(pageRequest);
+        return pagedList.map(UserDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO findById(Long id) {
+        Optional<User> obj = this.userRepository.findById(id);
+        User user = obj.orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
+        return new UserDTO(user);
+    }
+
+    @Transactional
+    public UserDTO createUser(UserInsertDTO newUser){
+        User newUserEntity = new User();
+        this.copyDtoToEntity(newUser, newUserEntity);
+        newUserEntity.setPassword(this.passwordEncoder.encode(newUser.getPassword()));
+        newUserEntity = this.userRepository.save(newUserEntity);
+        return new UserDTO(newUserEntity);
+    }
+
+    @Transactional
+    public UserDTO updateUser(
+            Long id,
+            UserDTO newUser){
+        try {
+            User userEntity = this.userRepository.getReferenceById(id);
+            this.copyDtoToEntity(newUser, userEntity);
+            userEntity = this.userRepository.save(userEntity);
+            return new UserDTO(userEntity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Id not found: " + id);
+        }
+    }
+
+    public void deleteUser(
+            Long id){
+        try{
+            this.userRepository.deleteById(id);
+        } catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException("Id not found: " + id);
+        } catch(DataIntegrityViolationException e){
+            throw new DatabaseException("Integrity violation");
+        }
+    }
+
+    private void copyDtoToEntity(UserDTO dto, User entity){
+        entity.setFirstName(dto.getFirstName());
+        entity.setLastName(dto.getLastName());
+        entity.setEmail(dto.getEmail());
+        entity.getRoles().clear();
+
+        for (RoleDTO roleDTO : dto.getRoles()){
+            Role role = this.roleRepository.getReferenceById(roleDTO.getId());
+            entity.getRoles().add(role);
+        }
+    }
+}
